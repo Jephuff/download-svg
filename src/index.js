d593360a86463741e01792bb7578ac3bc916d2c0
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-06-15 09:47:30 -0400

    don't create blob url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import saveSvgAsPng from 'save-svg-as-png';
 import canvg from '@jephuff/canvg';
-import base64toblob from 'base64toblob';
 
 const fontRegEx = /(font-size: ?)(([0-9]*(\.[0-9]+))?([^;]*))/;
 
@@ -13,16 +12,6 @@ const defaultConfig = {
   },
 };
 
-function downloadSvgUri(fileName, uri) {
-  if (uri.length <= 2000) return saveSvgAsPng.download(fileName, uri);
-
-  const contentType = 'image/svg+xml';
-  const data = uri.replace('data:image/svg+xml;base64,', '');
-  const blob = base64toblob(data, contentType);
-  const blobUrl = URL.createObjectURL(blob);
-  return saveSvgAsPng.download(fileName, blobUrl);
-}
-
 function downloadSvg({ svg, stylePrefix, fileName = 'export.svg', modifyFontFace, useCanvg }) {
   const selectorRemap = stylePrefix ? s => s.replace(stylePrefix, '').replace(/^ /, '') : undefined;
   const extension = (fileName.match(/\.[^.]*$/) || [])[0];
@@ -31,7 +20,7 @@ function downloadSvg({ svg, stylePrefix, fileName = 'export.svg', modifyFontFace
     saveSvgAsPng.svgAsDataUri(svg, {
       ...defaultConfig,
       selectorRemap,
-    }, uri => downloadSvgUri(fileName, uri));
+    }, uri => saveSvgAsPng.download(fileName, uri));
   } else {
     let encoderType;
     switch (extension) {
